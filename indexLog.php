f542709563a221477703899005316b4d97353354
Alejandro Enrique Jiménez Montenegro <aejimenezm@mineco.gob.gt>
2019-05-13 16:32:42 -0600

    13052019

---
+++ indexLog.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<HTML>
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>4Identity Authentication</title>
+	</head>
+	<body>
+		<form class="bit4id-auth form-stacked" method="post" action="home">
+			<div class="bit4id-authReq" style="display: none;">
+				<div class="bit4id-challenge">4IDENTITYCH</div>
+				<div class="bit4id-certType">ANY</div>
+				<div class="bit4id-certInfo">CN</div>
+			</div>
+			<input type="submit" value="Authenticate"/>
+		</form>
+		<script src="https://www.4identity.eu/smartengine/bit4id-sign.min.js">			
+		</script>
+	</body>
+</HTML>
